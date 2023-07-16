5473669fbe440d2d68d8ecc0ed2316655d496f3c
morales <juan.morales2004@hotmail.com>
2023-07-16 15:37:52 -0500

    se agrega la logica para el consumo de api comentado

--- src/app/productspage/productspage.component.ts
+++ src/app/productspage/productspage.component.ts
@@ -1,35 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core'; // Importa los módulos Component y OnInit desde @angular/core
+import { HttpClient } from '@angular/common/http'; // Importa el módulo HttpClient desde @angular/common/http
 
-interface Shirt {
-  name: string;
-  price: number;
-  image: string;
+interface Shirt { // Define una interfaz llamada Shirt
+  name: string; // Propiedad name de tipo string
+  price: number; // Propiedad price de tipo number
+  image: string; // Propiedad image de tipo string
 }
 
-@Component({
-  selector: 'app-productspage',
-  templateUrl: './productspage.component.html',
-  styleUrls: ['./productspage.component.css']
+@Component({ // Define el componente ProductspageComponent
+  selector: 'app-productspage', // Selector del componente
+  templateUrl: './productspage.component.html', // Plantilla del componente
+  styleUrls: ['./productspage.component.css'] // Estilos del componente
 })
-export class ProductspageComponent implements OnInit {
-  shirts: Shirt[] = [];
+export class ProductspageComponent implements OnInit { // Clase del componente que implementa OnInit
+  shirts: Shirt[] = []; // Propiedad shirts que es una matriz de objetos Shirt
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {} // Constructor del componente con una dependencia HttpClient
 
-  ngOnInit() {
-    this.getDataFromAPI();
+  ngOnInit() { // Método ngOnInit que se ejecuta cuando el componente se inicia
+    this.getDataFromAPI(); // Llama al método getDataFromAPI para obtener datos de la API
   }
 
-  getDataFromAPI() {
+  getDataFromAPI() { // Método para obtener datos de la API
     const url = 'https://koajstoreapi.onrender.com/api/products'; // Reemplaza con la URL real de la API
 
-    this.http.get<Shirt[]>(url).subscribe(
-      (data) => {
-        this.shirts = data;
+    this.http.get<Shirt[]>(url).subscribe( // Realiza una solicitud GET a la API y suscribe a la respuesta
+      (data) => { // Función de callback para la respuesta exitosa
+        this.shirts = data; // Asigna los datos recibidos a la propiedad shirts
       },
-      (error) => {
-        console.error('Error al obtener los datos:', error);
+      (error) => { // Función de callback para el manejo de errores
+        console.error('Error al obtener los datos:', error); // Muestra un mensaje de error en la consola
       }
     );
   }
