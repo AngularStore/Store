07757c1bc54b362527dd0fff4b13d391f425dad7
Santiago Jaramillo Duque <s.jaramillo4@utp.edu.co>
2023-07-16 18:14:23 -0500

    Agregamos comentarios a los archivos

--- src/app/Components/successful-pay/successfulPay.component.ts
+++ src/app/Components/successful-pay/successfulPay.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';// import component
 
-@Component({
-  selector: 'app-successful-pay',
-  templateUrl: './successfulPay.component.html',
-  styleUrls: ['./successfulPay.component.css']
+@Component({// decorator
+  selector: 'app-successful-pay',// se define el selector
+  templateUrl: './successfulPay.component.html',// se define la ruta del archivo html
+  styleUrls: ['./successfulPay.component.css']// se define la ruta del archivo css
 })
-export class SuccessfulPayComponent {
+export class SuccessfulPayComponent {// se define la clase
 
 }
