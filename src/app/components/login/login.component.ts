d155115f63aa0cefb7868ac8ca3d0aaaf84c21b4
JuanDavidGanan <gananmarinjuandavid@gmail.com>
2023-07-16 13:49:44 -0500

    Documentacion de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,19 +1,18 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../auth.service';
 
+//Sse utiliza para decorar una clase y definir un componente. Proporciona metadatos que describen cómo se debe comportar y presentar el componente.
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  username: string = '';
-  password: string = '';
-
-  constructor(private authService: AuthService) {}
-
-  login(): void {
-    this.authService.login(this.username, this.password)
+  username: string = ''; // Variable para almacenar el nombre de usuario ingresado en el formulario
+  password: string = ''; // Variable para almacenar la contraseña ingresada en el formulario
+   constructor(private authService: AuthService) {} // Inyección de dependencia del servicio AuthService
+   login(): void {
+    this.authService.login(this.username, this.password) // Llamada al método de inicio de sesión del servicio AuthService
       .subscribe(
         response => {
           // Aquí puedes manejar la respuesta de la API después de verificar el acceso
@@ -25,5 +24,4 @@ export class LoginComponent {
         }
       );
   }
-
 }
