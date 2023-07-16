50b71be5d1a608e40c8d6b2fb806310ed8aa534d
Adrián Fernando Gaitán Londoño <adriancho917373@gmail.com>
2023-07-16 14:05:25 -0500

    Structure to import Angular Material Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material-import/material.module';
 import { AppComponent } from './app.component';
-import { CreateProductComponent } from './create-product/create-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CreateProductComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
