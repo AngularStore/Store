88610f84eecc79d2c9c0f704f3c9a05bbf257795
Brayan <b.catano@utp.edu.co>
2023-07-17 15:00:57 -0500

    Enrutamientos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductspageComponent} from './components/productspage/productspage.component';
+import { RegisterComponent} from './components/register/register.component';
+import { SuccessfulPayComponent} from './components/successful-pay/successfulPay.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'products', component: ProductspageComponent},
+  {path: 'register', component: RegisterComponent},
+];
 
 
 
