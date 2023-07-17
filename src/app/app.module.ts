c80cfd0eac64dbf39cd20830c135cb58802d99cb
Adriancho91s <adriancho917373@gmail.com>
2023-07-17 17:03:58 -0500

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,26 @@ import { HttpClient } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 import { AboutComponent } from './components/about/about.component';
-import { ProductspageComponent } from './productspage/productspage.component';
+import { ProductspageComponent } from './components/productspage/productspage.component';
+import { ProductPageComponent } from './components/product-page/product-page.component';
+import { CreateProductComponent } from './components/create-product/create-product.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NotFoundComponent } from './components/notFound/notFound.component';
+import { SuccessfulPayComponent } from './components/successful-pay/successfulPay.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule} from '@angular/material/menu';
+import { MatIconModule} from '@angular/material/icon';
+import { MatButtonModule} from '@angular/material/button';
+import { MatTableModule} from '@angular/material/table';
+import { MatPaginatorModule} from '@angular/material/paginator';
+import { MatSortModule} from '@angular/material/sort';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatOptionModule} from '@angular/material/core';
+import { MatSelectModule} from '@angular/material/select';
+import { MatInputModule} from '@angular/material/input';
+import { ProductFormComponent } from './components/product-form/product-form.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +45,8 @@ import { ProductspageComponent } from './productspage/productspage.component';
     CreateProductComponent,
     FooterComponent,
     NotFoundComponent,
-    SuccessfulPayComponent
+    SuccessfulPayComponent,
+    ProductFormComponent
   ],
   imports: [
     FormsModule,
