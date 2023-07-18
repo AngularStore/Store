74891e4802917cb30840f0076dc10991af151e3f
Brayan Cataño Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2023-07-17 23:31:11 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-const routes: Routes = [];
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent} from './components/logout/logout.component';
+import { ProductspageComponent} from './components/productspage/productspage.component';
+import { RegisterComponent} from './components/register/register.component';
+import { ProductPageComponent } from './components/product-page/product-page.component';
+import { SuccessfulPayComponent} from './components/successful-pay/successfulPay.component';
+import { TokenGuard } from './guard/auth.guard';
+import { TokenGuardAdmin } from './guard/admin.guard';
+import { ProductFormComponent } from './components/product-form/product-form.component';
+import { WelcomeComponent} from './components/welcome/welcome.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'logout', component: LogoutComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'products', component: ProductspageComponent},
+  {path: 'products/:id', component:  ProductPageComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'welcome', component: WelcomeComponent},
+];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
