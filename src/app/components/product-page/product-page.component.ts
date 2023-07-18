b9e8ba1b98172c105312a52f5144ae995f8a78ee
Brayan Cataño Giraldo <135563563+brayangiraldo02@users.noreply.github.com>
2023-07-18 09:31:00 -0500

    Update product-page.component.ts

--- src/app/components/product-page/product-page.component.ts
+++ src/app/components/product-page/product-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
+import { LocalStorageService } from 'src/app/local-storage.service';
 
 @Component({
   selector: 'app-product-page',
